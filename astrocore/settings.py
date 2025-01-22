46a76261ca4a6feaf8c2088a73d932cf15e1d020
Abhinav <ingeniousabhinav@gmail.com>
2025-01-22 20:46:14 +0530

    Improved from render docs

--- astrocore/settings.py
+++ astrocore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-k@xb@($_a&++bv5s@7pp6v#t$h9-!+wmi0)zw=2l)019)2=9bx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jain-astro-counselling.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['jain-astro-counselling.onrender.com', 'localhost','127.0.0.1']
 
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -78,13 +79,14 @@ WSGI_APPLICATION = 'astrocore.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://ingeniousabhinav:7XmpUOShqRxaMqBJMGOutUlikmF1jnir@dpg-cu880jdds78s73a4oacg-a.oregon-postgres.render.com/astrocounselling',
+        conn_max_age=600
+    )
 }
 
-DATABASES["default"] = dj_database_url.parse("postgresql://ingeniousabhinav:7XmpUOShqRxaMqBJMGOutUlikmF1jnir@dpg-cu880jdds78s73a4oacg-a.oregon-postgres.render.com/astrocounselling")
+# DATABASES["default"] = dj_database_url.parse("postgresql://ingeniousabhinav:7XmpUOShqRxaMqBJMGOutUlikmF1jnir@dpg-cu880jdds78s73a4oacg-a.oregon-postgres.render.com/astrocounselling")
 
 
 # Password validation
@@ -121,11 +123,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
